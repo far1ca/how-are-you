0187ffc592913daa537c855fddccede142ae149b
far1ca <77165212+far1ca@users.noreply.github.com>
2025-10-05 14:29:20 +0200

    Add starting logic for response

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -10,6 +10,9 @@ import { EmotionService } from './emotion.service';
 export class AppComponent implements OnInit {
   text: string = '';
   inputDisabled: boolean = true;
+  showResult: boolean = false;
+  emotion: string = '';
+  isAnalyzing: boolean = false;
   result: any;
 
   constructor(private emotionService: EmotionService) {}
@@ -21,6 +24,34 @@ export class AppComponent implements OnInit {
   }
 
   async analyze() {
+    this.isAnalyzing = true;
     this.result = await this.emotionService.analyze(this.text);
+    if (this.result) {
+      console.log(this.result);
+      this.isAnalyzing = false;
+      switch (this.result[0][0].label) {
+        case 'joy':
+          this.emotion = 'happy';
+          break;
+        case 'sadness':
+          this.emotion = 'sad';
+          break;
+        case 'anger':
+          this.emotion = 'angry';
+          break;
+        case 'fear':
+          this.emotion = 'scared';
+          break;
+        case 'surprise':
+          this.emotion = 'surprised';
+          break;
+        case 'disgust':
+          this.emotion = 'disgusted';
+          break;
+        default:
+          this.emotion = 'neutral';
+      }
+      this.showResult = true;
+    }
   }
 }
